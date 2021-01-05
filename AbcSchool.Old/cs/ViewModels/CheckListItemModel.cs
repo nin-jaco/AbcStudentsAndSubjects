3c86bed3724a0f0db078b154486ff0db2dc9279d
Jaco <jaco.kleynhans@icloud.com>
2021-01-05 10:17:08 +0200

    project cleanup

--- AbcSchool.Old/cs/ViewModels/CheckListItemModel.cs
+++
@@ -1,9 +0,0 @@
-﻿namespace ABCSchool.Uwp.ViewModels
-{
-    public class CheckListItemModel
-    {
-        public int Id { get; set; }
-        public string Text { get; set; }
-        public bool IsSelected { get; set; } = false;
-    }
-}
